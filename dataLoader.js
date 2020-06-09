ae80a4c3dad3b827eab19b51e3825536b7d6b43e
jeremybrooks <jeremybrooks321@gmail.com>
2020-06-09 09:25:42 +0100

    basic dropdown setup

---
+++ dataLoader.js
@@ -0,0 +1,30 @@
+createLists = () => {
+  let yearsToCreate = 41;
+  let monthsToCreate = ["1023", "1123", "1223", "0123", "0223", "0323"];
+  let forecastVariables = ["z500", "abc", "cde"];
+
+  let yearDropDown = document.getElementById("yearDropDown");
+  let dateDropDown = document.getElementById("dateDropDown");
+  let forecastVariableDropdown = document.getElementById(
+    "forecastVariableDropDown"
+  );
+
+  for (let yearCount = yearsToCreate; yearCount > 0; yearCount--) {
+    let option = document.createElement("option");
+    let now = new Date();
+    option.text = now.getFullYear() - yearCount;
+    yearDropDown.appendChild(option);
+  }
+
+  monthsToCreate.forEach((month) => {
+    let option = document.createElement("option");
+    option.text = month;
+    dateDropDown.appendChild(option);
+  });
+
+  forecastVariables.forEach((forecastVariable) => {
+    let option = document.createElement("option");
+    option.text = forecastVariable;
+    forecastVariableDropdown.appendChild(option);
+  });
+};
